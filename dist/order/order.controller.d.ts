f54c3889ebb7e5bb000e263a0c9e79436b49be09
Alex <yen@virtualinternships.com>
2024-04-27 16:03:52 +0700

    up

--- dist/order/order.controller.d.ts
+++ dist/order/order.controller.d.ts
@@ -3,7 +3,8 @@ export declare class OrderController {
     private readonly orderService;
     constructor(orderService: OrderService);
     getOrderNumber(): Promise<number>;
-    orders(): Promise<import("../model/order.entity").Order[]>;
+    orders(body: any): Promise<import("../model/order.entity").Order[]>;
+    orderReport(body: any): Promise<any>;
     create(body: any, req: any): Promise<any>;
     order(param: any): Promise<import("../model/order.entity").Order>;
 }
--- dist/order/order.service.d.ts
+++ dist/order/order.service.d.ts
@@ -8,7 +8,8 @@ export declare class OrderService {
     constructor(orderRepository: Repository<Order>, configAppRepository: Repository<ConfigApp>, dataSource: DataSource);
     findByOrderNumber(): Promise<number | undefined>;
     findById(id: number): Promise<Order | undefined>;
-    find(): Promise<Order[] | undefined>;
+    find(body: any): Promise<Order[] | undefined>;
+    orderReport(body: any): Promise<any | undefined>;
     create(body: any): Promise<any>;
     update(body: any): Promise<any>;
 }
